433f048ef0b91513ba94f644aa00714c42afa47d
99yaswanth <yashwanth24k@gmail.com>
2022-01-12 22:13:26 +0530

    added deploy file

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -3,22 +3,25 @@ pipeline{
     parameters{
         string(name: 'BRANCH' , defaultValue: 'master' , description: '')
         string(name: 'serverip' , defaultValue: ' ' , description: '')
-        string(name: 'Build' , defaultValue: ' ' , description: '')
+        string(name: 'Buildno' , defaultValue: ' ' , description: '')
 
     }
     stages{
         stage("download artifacts from s3"){
             steps{
                 println "download artifacts"
-                sh "aws s3 cp s3://yashwanth24/${BRANCH}/hello-${Build}.war ."
+                sh """
+                aws s3 ls
+                aws s3 cp s3://yashwanth24/${BRANCH}/hello-${Buildno}.war .
+                """
 
             }
         }
         stage("copy artifacts"){
             steps{
                 println "copying artifacts to tomcat"
-                sh "scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem hello-${Build}.war ec2-user@${serverip}:/tmp"
-                sh "ssh -i /tmp/nvirginia1.pem ec2-user@${serverip} \"sudo cp /tmp/hello-${Build}.war /var/lib/tomcat/webapps\""
+                sh "scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem hello-${Buildno}.war ec2-user@${serverip}:/tmp"
+                sh "ssh -i /tmp/nvirginia1.pem ec2-user@${serverip} \"sudo cp /tmp/hello-${Buildno}.war /var/lib/tomcat/webapps\""
             }
         }
     }
