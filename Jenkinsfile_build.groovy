f9198bf733c85a15925ff7433b30cad7ffcd5b78
99yaswanth <yashwanth24k@gmail.com>
2022-01-12 21:24:21 +0530

    added build file

---
+++ Jenkinsfile_build.groovy
@@ -0,0 +1,28 @@
+pipeline{
+    agent any
+    parameters{
+        String(name: 'BRANCH', defaultValue: 'master' , description: '')
+
+    }
+    stages{
+        stage("checkout code"){
+            steps{
+                println "clone a clode"
+                git branch: "${BRANCH}",
+                url: 'https://github.com/KuruvaSomaSekhar/nov21.git'
+            }
+        }
+        stage("build code"){
+            steps{
+                println "build code"
+                sh "mvn clean package"
+                sh "ls -l"
+            }
+        }
+        stage("upload to s3"){
+            steps{
+                sh "aws s3 cp target/hello-*.war s3://yashwanth24/${BRANCH}/"
+            }
+        }
+    }
+}
